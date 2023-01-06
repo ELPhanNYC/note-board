c114013607395232f444e1324fd555303ef03659
elphan <starsoccerethan@gmail.com>
2023-01-05 21:36:25 -0500

    Added changes that allows user to change the color of the notes

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 let active_tasks = [];
+//let task_button = [];
 let active_count = 0;
+let colors = ["lightblue", "pink", "yellow","#ccccff"];
+let current_color = "yellow";
+function taskColor(color_index){
+    current_color = colors[color_index];
+    if(active_tasks.length > 0){
+        for(let i of active_tasks){
+            i.setAttribute("style","background-color: "+current_color+";");
+        }
+    }    
+}
 function delete_div(div_name){
     let div = document.getElementById(div_name);
+    //active_tasks.remove(div);
     div.remove();
 }
 function create_div(){
@@ -9,9 +21,11 @@ function create_div(){
     let new_div = document.createElement("div");
     let div_name =  "task-"+active_count;
     new_div.setAttribute("id",div_name);
+    new_div.setAttribute("style","background-color: "+current_color+";");
     let new_p = document.createElement("p");
     let delete_button = document.createElement("button");
     delete_button.setAttribute("class","x")
+    //delete_button.setAttribute("style","background-color: "+current_color+";");
     delete_button.onclick = function(){
         delete_div(div_name);
     };
